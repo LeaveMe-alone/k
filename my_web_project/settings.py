1f6af9d9c3920b718ab1b8ad83dcdb59498582ea
LeaveMe-alone <114977976+LeaveMe-alone@users.noreply.github.com>
2024-03-01 21:38:23 +0300

    L

--- my_web_project/settings.py
+++ my_web_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x3+=pfw%vgq(8_bia9au1o--jkhe50falj-@*zjxwm78qnf86y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['20.246.131.194', 'barrackweb.azurewebsites.net']
+ALLOWED_HOSTS = ['20.246.131.194', 'barrackouma.azurewebsites.net']
 # settings.py
 
 CSRF_TRUSTED_ORIGINS = ["barrackouma.azurewebsites.net"]
